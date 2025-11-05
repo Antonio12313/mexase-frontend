15c5cb5e356eb747c8301f914d70ce5bca3cfc37
antonioreis <antonio.augustosreis@gmail.com>
2025-11-05 14:16:26 -0300

    Feature: removendo espaço

--- src/app/(dashboard)/paciente/page.tsx
+++ src/app/(dashboard)/paciente/page.tsx
@@ -221,7 +221,6 @@ export default function Page() {
                                     ))}
                                 </TableBody>
                             </Table>
-                            
                         )}
 
                     </div>
